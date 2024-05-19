35261328c39761c7f31279b150370522be2feebf
zeldamaster911 <zeldamaster911@gmail.com>
2024-05-18 21:25:03 -0600

    Auto-commit from script

---
+++ gitauto.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Pull the latest changes from the remote repository
+echo Pulling the latest changes from the remote repository...
+git pull origin master
+
+REM Add all changes to the staging area
+echo Adding changes to the staging area...
+git add .
+
+REM Commit the changes
+echo Committing changes...
+git commit -m "Auto-commit from script"
+
+REM Push the changes to the remote repository
+echo Pushing changes to the remote repository...
+git push origin master
+
+echo Done!
+pause

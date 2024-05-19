83f57cd7f226b56a097e6079ad3671328dc2455d
zeldamaster911 <zeldamaster911@gmail.com>
2024-05-19 17:25:07 -0600

    Auto-commit from script

--- gitauto.bat
+++ gitauto.bat
@@ -1,22 +1,3 @@
-@echo off
-REM Change directory to your repository
-cd /d "C:\Users\user1\3d zeldaish"
-
-REM Pull the latest changes from the remote repository
-echo Pulling the latest changes from the remote repository...
-git pull origin main
-
-REM Add all changes to the staging area
-echo Adding changes to the staging area...
-git add .
-
-REM Commit the changes
-echo Committing changes...
-git commit -m "Auto-commit from script"
-
-REM Push the changes to the remote repository
-echo Pushing changes to the remote repository...
-git push origin main
-
-echo Done!
-pause
+version https://git-lfs.github.com/spec/v1
+oid sha256:78ce198ff7d8d3158e1c9ef3c2ae706c891e833f91723009681badaabf21b281
+size 564

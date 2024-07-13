64a9a5d3e54525f3913f17eecab4434e03fa5d54
zeldamaster911 <zeldamaster911@gmail.com>
2024-07-12 22:41:22 -0600

    Auto-commit from script

--- Small Multiplayer/Assets/Scripts (Net)/Objects/Spin.cs
+++ Small Multiplayer/Assets/Scripts (Net)/Objects/Spin.cs
@@ -1,16 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 
-public class Spin : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NetworkRigidbody))]
+[RequireComponent(typeof(NetworkTransform))]
+public class Spin : NetworkBehaviour
 {
+    public float spinSpeed;
+    public Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void OnNetworkSpawn()
     {
-        
+        base.OnNetworkSpawn();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up, spinSpeed);
+        rb.rotation = transform.rotation;
     }
 }

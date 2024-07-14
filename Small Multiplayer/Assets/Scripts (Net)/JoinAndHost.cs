043136b1fe099d70b25f106213d50330a3e7f87f
zeldamaster911 <zeldamaster911@gmail.com>
2024-07-13 18:22:43 -0600

    Auto-commit from script

--- Small Multiplayer/Assets/Scripts (Net)/JoinAndHost.cs
+++ Small Multiplayer/Assets/Scripts (Net)/JoinAndHost.cs
@@ -7,11 +7,11 @@ using UnityEngine.UIElements;
 public class JoinAndHost : MonoBehaviour
 {
     // Public variable to store the player's network index
-
+    UnityTransport transport;
     private void Start()
     {
         // Set the network transport address and port
-        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         transport.SetConnectionData(transform.FindDeepChild("IP").GetComponent<TMP_InputField>().text, ushort.Parse( transform.FindDeepChild("Port").GetComponent<TMP_InputField>().text));
     }
 
@@ -19,12 +19,14 @@ public class JoinAndHost : MonoBehaviour
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        transport.SetConnectionData(transform.FindDeepChild("IP").GetComponent<TMP_InputField>().text, ushort.Parse(transform.FindDeepChild("Port").GetComponent<TMP_InputField>().text));
     }
 
     // Method to start the game as client
     public void JoinGame()
     {
         NetworkManager.Singleton.StartClient();
+        transport.SetConnectionData(transform.FindDeepChild("IP").GetComponent<TMP_InputField>().text, ushort.Parse(transform.FindDeepChild("Port").GetComponent<TMP_InputField>().text));
     }
 
     // Method to be called when the network object spawns

bfd0adc19b77ff5fccf9ec66f440751ba096f923
zeldamaster911 <zeldamaster911@gmail.com>
2024-07-16 22:25:10 -0600

    Auto-commit from script

--- Small Multiplayer/Assets/Scripts (Net)/Player/NameTag.cs
+++ Small Multiplayer/Assets/Scripts (Net)/Player/NameTag.cs
@@ -4,26 +4,33 @@ using Unity.Netcode.Transports.UTP;
 using TMPro;
 using System.Collections.Generic;
 using System;
+using Unity.Collections;
 
+[GenerateSerializationForType(typeof(string))]
 public class NameTag : NetworkBehaviour
 {
-    public string Name;
+    public NetworkVariable<FixedString64Bytes> Name = new NetworkVariable<FixedString64Bytes>();
+    public string localname;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         Start();
     }
+
     public void Start()
     {
         if (IsOwner)
         {
-            name = PlayerPrefs.GetString("Name", "Toast");
+            Name.Value = PlayerPrefs.GetString("Name", "Toast");
+            localname = Name.Value.ToString();
         }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+        // Update logic (if any) goes here
     }
 }
--- Small Multiplayer_clone_0/Assets/Scripts (Net)/Network&Game Management/CurrentSessionStats.cs
+++ Small Multiplayer_clone_0/Assets/Scripts (Net)/Network&Game Management/CurrentSessionStats.cs
@@ -4,16 +4,51 @@ using Unity.Netcode.Transports.UTP;
 using TMPro;
 using System.Collections.Generic;
 using System;
-using UnityEditor.Build;
+using UnityEngine.Rendering;
+using Unity.VisualScripting;
+using AYellowpaper.SerializedCollections;
+using Unity.Collections;
 
-[Serializable]
-public struct PlayerDataInspector
+[System.Serializable]
+// Define a struct named PlayerData
+public struct PlayerData : IEquatable<PlayerData>, INetworkSerializeByMemcpy
+
 {
+    // A unique identifier for the client
     public ulong clientId;
-    public string name;
-    public GameObject PlayerParent;
+
+    // A fixed-size string for the player's name
+    public FixedString64Bytes name;
+
+
+    // Implementation of the IEquatable<PlayerData> interface to compare two PlayerData instances
+    public bool Equals(PlayerData other)
+    {
+        // Check if clientId, name, and PlayerParentId are equal between this instance and the other instance
+        return clientId == other.clientId && name.Equals(other.name);
+    }
+
+    // Override the Equals method to compare with an object
+    public override bool Equals(object obj)
+    {
+        // Check if the object is a PlayerData instance and compare using the Equals method
+        return obj is PlayerData other && Equals(other);
+    }
+
+    // Override the GetHashCode method to provide a hash code for this instance
+    public override int GetHashCode()
+    {
+        // Generate a hash code using a tuple containing clientId, name, and PlayerParentId
+        return (clientId, name).GetHashCode();
+    }
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref clientId);
+        serializer.SerializeValue(ref name);
+        // Serialize other fields...
+    }
 }
-[GenerateSerializationForTypeAttribute(typeof(PlayerDataInspector))]
+[GenerateSerializationForType(typeof(PlayerData))]
 public class CurrentSessionStats : NetworkBehaviour
 {
     public static CurrentSessionStats Instance { get; private set; }
@@ -24,7 +59,7 @@ public class CurrentSessionStats : NetworkBehaviour
     public float IndexOfRunner2;
     public float IndexOfRunner3;
     public UnityTransport transport;
-    public NetworkManager nm = NetworkManager.Singleton;
+    public NetworkManager nm;
     public GameObject playertemp;
 
     public enum GameStateEnum
@@ -38,14 +73,85 @@ public class CurrentSessionStats : NetworkBehaviour
     {
         Standard
     }
+    public NetworkVariable<GameModeEnum> GameMode = new NetworkVariable<GameModeEnum>();
+    public NetworkVariable<GameStateEnum> GameState = new NetworkVariable<GameStateEnum>();
+    public NetworkList<PlayerData> playersList = new NetworkList<PlayerData>();
 
-    public NetworkVariable<GameStateEnum> GameState = new NetworkVariable<GameStateEnum>(GameStateEnum.UI);
-    public NetworkVariable<GameModeEnum> GameMode = new NetworkVariable<GameModeEnum>(GameModeEnum.Standard);
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        nm = NetworkManager.Singleton;
+        Instance = this;
+    }
+    public void Update()
+    {
+        if (IsServer)
+        {
 
+            GetPlayers();
+            UpdateGame();
+            Debug.Log("I am Server");
+            
+        }
+    }
+    public void UpdateGame()
+    {
+        switch (GameMode.Value)
+        {
+            case GameModeEnum.Standard:
+                break;
+        }
 
-    [NonSerialized]
-    public NetworkVariable<List<PlayerDataInspector>> playersList = new NetworkVariable<List<PlayerDataInspector>>();
-    public List<PlayerDataInspector> PlayersListInspector;
+        switch (GameState.Value)
+        {
+            case GameStateEnum.InGame:
+                break;
 
-   
+            case GameStateEnum.UI:
+                foreach (PlayerData player in playersList)
+                {
+                    var playerObj = nm.ConnectedClientsList[(int)player.clientId].PlayerObject.gameObject;
+                    playerObj.GetComponentInChildren<Rigidbody>().isKinematic = true;
+                    playerObj.GetComponentInChildren<PlayerMovement>().enabled = false;
+                }
+                break;
+
+            case GameStateEnum.Other:
+                break;
+        }
+    }
+    public void GetPlayers()
+    {
+        if (IsServer)
+        {
+            playersList.Clear(); // Ensure we start fresh each time we get players
+
+            foreach (var player in nm.ConnectedClientsList)
+            {
+                var playerObj = player.PlayerObject;
+                var nameTag = playerObj.gameObject.GetComponentInChildren<NameTag>(false);
+
+                if (nameTag != null)
+                {
+                    string name = nameTag.Name.Value.ToString();
+                    PlayerData p = new PlayerData
+                    {
+                        clientId = playerObj.OwnerClientId,
+                        name = name
+                    };
+
+                    if (!playersList.Contains(p))
+                    {
+                        playersList.Add(p);
+                        Debug.Log(p.name.ToString());
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"PlayerObject for ClientId {playerObj.OwnerClientId} does not have a NameTag component.");
+                }
+            }
+           
+        }
+    }
 }

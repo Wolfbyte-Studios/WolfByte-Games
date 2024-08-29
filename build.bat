e0a1643da97004fedaff6e58a2915a8976d404d4
zeldamaster911 <zeldamaster911@gmail.com>
2024-08-28 18:15:00 -0600

    Auto-commit from script

--- build.bat
+++ build.bat
@@ -2,8 +2,13 @@
 SET UNITY_PATH="C:\Program Files\Unity\Hub\Editor\6000.0.5f1\Editor\Unity.exe"
 SET PROJECT_PATH="C:\Users\user1\3d zeldaish\Ruin The Runner"
 SET BUILD_TARGET="C:\Users\user1\3d zeldaish\Builds\Ruin The Runner Current\Ruin The Runner.exe"
+SET ARCHIVE_PATH="C:\Users\user1\3d zeldaish\Builds\Ruin_The_Runner_Current.zip"
 
-%UNITY_PATH% -quit -batchmode -buildWindows64Player %BUILD_TARGET%
+%UNITY_PATH% -quit -batchmode -buildWindows64Player "%BUILD_TARGET%"
 
-echo Build completed.
+echo Build completed. Creating archive...
 
+REM Creating a zip archive of the build
+powershell -Command "Compress-Archive -Path 'C:\Users\user1\3d zeldaish\Builds\Ruin The Runner Current\*' -DestinationPath '%ARCHIVE_PATH%'"
+
+echo Archive created at %ARCHIVE_PATH%

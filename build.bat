ad6e7dd4c4e4d91562b23872341d591e41a597f4
zeldamaster911 <zeldamaster911@gmail.com>
2024-08-30 19:36:05 -0600

    Auto-commit from script

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ SET PROJECT_PATH="C:\Users\user1\3d zeldaish\Ruin The Runner"
 SET BUILD_TARGET="C:\Users\user1\3d zeldaish\Builds\Ruin The Runner Current\Ruin The Runner.exe"
 SET ARCHIVE_PATH="C:\Users\user1\3d zeldaish\Builds\Ruin_The_Runner_Current.zip"
 
-%UNITY_PATH% -quit -batchmode -buildWindows64Player "%BUILD_TARGET%"
+%UNITY_PATH% -quit -buildWindows64Player "%BUILD_TARGET%"
 
 echo Build completed. Creating archive...
 

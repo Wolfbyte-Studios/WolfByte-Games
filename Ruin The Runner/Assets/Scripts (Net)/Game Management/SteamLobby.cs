7b541a9cf46be4485ad90bd96e9e7e3f260ed5b2
zeldamaster911 <zeldamaster911@gmail.com>
2024-09-01 23:41:38 -0600

    xc

--- Ruin The Runner/Assets/Scripts (Net)/Game Management/SteamLobby.cs
+++ Ruin The Runner/Assets/Scripts (Net)/Game Management/SteamLobby.cs
@@ -1,3 +1,87 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d106622d44c1e36a950a5de4ddcf56190aa342b67cad29c1b35d56252bde061
-size 1983
+using UnityEngine;
+using Mirror;
+using Steamworks;
+
+public class SteamLobby : MonoBehaviour
+{
+
+    public GameObject hostButton = null;
+    public GameObject disableThis;
+
+
+    private NetworkManager networkManager;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private const string HostAddressKey = "HostAddress";
+
+
+    private void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+
+        if (!SteamManager.Initialized) { return; }
+
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+
+    }
+
+
+
+    public void HostLobby()
+    {
+
+        hostButton.SetActive(false);
+
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+
+    }
+
+
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            hostButton.SetActive(true);
+            return;
+        }
+
+
+        networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey, SteamUser.GetSteamID().ToString());
+
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+
+        if (NetworkServer.active)
+        {
+            return;
+        }
+
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
+
+        networkManager.networkAddress = hostAddress;
+        networkManager.StartClient();
+
+        hostButton.SetActive(false);
+        disableThis.SetActive(false);
+
+    }
+
+
+}
